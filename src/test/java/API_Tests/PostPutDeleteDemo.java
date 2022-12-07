84941496f45a20b5898a19e6e2dea6b08b41fd3f
Tushar Varma <tushar.varma@Tushars-MacBook-Pro.local>
2022-12-07 13:02:31 +0530

    Added the feeder which generates random string in Post Call

--- src/test/java/API_Tests/PostPutDeleteDemo.java
+++ src/test/java/API_Tests/PostPutDeleteDemo.java
@@ -3,6 +3,11 @@ package API_Tests;
 import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
+import jodd.util.RandomString;
+
+import java.util.*;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.http;
@@ -10,45 +15,33 @@ import static io.gatling.javaapi.http.HttpDsl.status;
 
 public class PostPutDeleteDemo extends Simulation {
 
+    Iterator<Map<String, Object>> feeder =
+            Stream.generate((Supplier<Map<String, Object>>) ()
+                    -> {
+                String name = RandomString.get().random(6,"Test");
+                return Collections.singletonMap("username", name);
+                    }
+            ).iterator();
+
     //protocol
     private HttpProtocolBuilder httpProtocol = http
             .baseUrl("https://reqres.in/api");
 
     //sceanrio
-    private ScenarioBuilder Createusers = scenario("Create User").exec(
+    private ScenarioBuilder Createusers = scenario("Create User")
+            .feed(feeder)
+            .exec(
             http("Create a new User")
-                    .post("/users").header("content-type","application/json")
-                    .asJson()
-                    .body(RawFileBody("Data/user.json"))
-//                    .body(StringBody("{\n" +
-//                            "    \"name\": \"morpheus\",\n" +
-//                            "    \"job\": \"leader\"\n" +
-//                            "}")).asJson()
-                    .check(status().is(201),jsonPath("$.name").is("qaautomationhub"))).pause(1);
-
-    private ScenarioBuilder Upateusers = scenario("Update User").exec(
-            http("Update the User")
-                    .put("/users/2").header("content-type","application/json")
-                    .asJson()
-                    .body(RawFileBody("Data/user.json"))
-//
-                    .check(status().is(200),jsonPath("$.name").is("qaautomationhub")));
-
-    private ScenarioBuilder Deleteusers = scenario("Delete User").exec(
-            http("Delete the User")
-                    .delete("/users/2").header("content-type","application/json")
-                    .check(status().is(204)));
-    private ScenarioBuilder FetchUsers = scenario("Get API Request Demo").exec(
-            http("Get Single User")
-                    .get("/users/2").check(
-                            status().is(200)).
-                    check(jsonPath("$.data.first_name").is("Janet"))
-    ).pause(1);
+                    .post("/users")
+                    .header("content-type","application/json")
+                    .body(StringBody("{\"name\": \"${username}\",\"job\":\"leader\"}"))
+                    .check(status().is(201),jsonPath("$.job").is("leader"),jsonPath("$.name").saveAs("Username"))).pause(1)
+            .exec(session -> {
+                System.out.println(session.get("Username").toString());
+                return session;
+            });
     {
         setUp(
-                Createusers.injectOpen(rampUsers(5).during(5)),
-                Upateusers.injectOpen(rampUsers(10).during(7)),
-                Deleteusers.injectOpen(rampUsers(10).during(7))).protocols(httpProtocol);
-               FetchUsers.injectOpen(atOnceUsers(30)).protocols(httpProtocol);
+                Createusers.injectOpen(rampUsers(5).during(5)).protocols(httpProtocol));
     }
 }
